deb1a50af6ec1ee64a860dcbed14f8cfc309cc9f
Nikita Mikhaylov <nikita.mikhaylov@aimtech.com>
2020-07-17 17:08:19 +0300

    test 42

--- app/main.cpp
+++ app/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
 	const string playerKey(argv[2]);
 
 	cout << "ServerUrl: " << serverUrl << "; PlayerKey: " << playerKey << endl;
-	
+
 	const regex urlRegexp("http://(.+):(\\d+)");
 	smatch urlMatches;
 	if (!regex_search(serverUrl, urlMatches, urlRegexp) || urlMatches.size() != 3) {
@@ -19,14 +19,14 @@ int main(int argc, char* argv[])
 	const string serverName = urlMatches[1];
 	const int serverPort = stoi(urlMatches[2]);
 	httplib::Client client(serverName, serverPort);
-	const shared_ptr<httplib::Response> serverResponse = 
-		client.Post(serverUrl.c_str(), playerKey.c_str(), "text/plain");
+	const shared_ptr<httplib::Response> serverResponse =
+		client.Post(serverUrl.c_str(), "42", "text/plain");
 
 	if (!serverResponse) {
 		cout << "Unexpected server response:\nNo response from server" << endl;
 		return 1;
 	}
-	
+
 	if (serverResponse->status != 200) {
 		cout << "Unexpected server response:\nHTTP code: " << serverResponse->status
 		          << "\nResponse body: " << serverResponse->body << endl;

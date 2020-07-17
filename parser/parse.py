0bdef29e871cf5be4e0f6b8dd66c9c5551a84825
Alexander Golovanov <Golovanov12345@gmail.com>
2020-07-17 20:12:12 +0300

    Make text reprs

--- parser/parse.py
+++ parser/parse.py
@@ -25,7 +25,8 @@ def tokenize(a):
 	grid_tokens = list(map(f, grid_tokens))
 	return grid_tokens
 
-lmbd = "##\n.#"
+lmbd = "##\n#."
+eq = "###\n#..\n###"
 
 tokname = {
 	'.#\n#.': '0',
@@ -67,12 +68,86 @@ tokname = {
 	'######\n#....#\n#....#\n#....#\n#....#\n######': 'draw',
 }
 
+ntok = dict()
+for k, v in tokname.items():
+	if len(k) < 3:
+		ntok[k] = v
+		continue
+	g = k.split()
+	i = 0
+	while i + 3 < len(g[0]):
+		if g[0][i:i+3] != '##.':
+			break
+		if g[1][i:i+3] != '#..':
+			break
+		ok = True
+		for j in range(2, len(g)):
+			if g[j][i:i+3] != '...':
+				ok = False
+				break
+		if not ok:
+			break
+		i += 3
+	ntok["\n".join(x[i:] for x in g)] = v
+tokname = ntok
+
+def decode_number(g):
+	if len(g) == len(g[0]) and g[0][0] == '.' and g[0][1:] == '#' * (len(g[0]) - 1) and "".join(x[0] for x in g[1:]) == '#' * (len(g) - 1):
+		res = 0
+		cur = 1
+		for i in range(1, len(g[0])):
+			for j in range(1, len(g[0])):
+				if g[i][j] == '#':
+					res += cur
+				cur *= 2
+		return str(res)
+	elif len(g) == len(g[0]) + 1 and g[0][0] == '.' and g[0][1:] == '#' * (len(g[0]) - 1) and "".join(x[0] for x in g[1:]) == '#' * (len(g) - 1) and g[-1][1:] == '.' * (len(g[0]) - 1):
+		res = 0
+		cur = 1
+		for i in range(1, len(g[0])):
+			for j in range(1, len(g[0])):
+				if g[i][j] == '#':
+					res += cur
+				cur *= 2
+		return str(-res)
+	else:
+		return None
+
+def decode_var(g):
+	if len(g) != len(g[0]):
+		return None
+	sz = len(g)
+	if g[0] != '#' * sz or g[-1] != '#' * sz:
+		return None
+	if "".join(s[0] for s in g) != '#' * sz:
+		return None
+	if "".join(s[-1] for s in g) != '#' * sz:
+		return None
+	def invert(s):
+		return "".join('#' if x == '.' else '.' for x in s)
+	tmp = decode_number([invert(s[1:-1]) for s in g[1:-1]])
+	if tmp:
+		return 'x' + tmp
+	else:
+		return None
+
+def decode_grid(g):
+	gs = g.split()
+	if g in tokname:
+		return tokname[g]
+	elif decode_number(gs):
+		return decode_number(gs)
+	elif decode_var(gs):
+		return decode_var(gs)
+	else:
+		return '?'
+
 def build_tree(tokens):
 	arities = []
 	tkns = []
 	arity = 0
 	for token in tokens:
-		if token == lmbd:
+		if token == "lambda":
 			arity += 1
 		else:
 			tkns.append(token)
@@ -94,18 +169,42 @@ def repr(tkns, args):
 	sz = len(args)
 	reprs = ["" for i in range(sz)]
 	for i in range(sz - 1, -1, -1):
-		reprs[i] = tokname.get(tkns[i], '?')
+		reprs[i] = tkns[i]
 		if args[i]:
 			reprs[i] += "(" + ", ".join(reprs[j] for j in args[i]) + ")"
 	return reprs[0]
 
+def parse_hand_side(tokens):
+	tokens = list(map(decode_grid, tokens))
+	st = [[]]
+	for x in tokens:
+		if x == "left_bracket":
+			st[-1].append([])
+			st.append([])
+		elif x == "right_bracket":
+			st[-2][-1].append(repr(*build_tree(st[-1])))
+			st.pop()
+			st[-1][-1] = "[" + ", ".join(st[-1][-1]) + "]"
+		elif x == "delim":
+			st[-2][-1].append(repr(*build_tree(st[-1])))
+			st[-1] = []
+		else:
+			st[-1].append(x)
+	return repr(*build_tree(st[0]))
+
 def parse(grid):
-	return repr(*build_tree(tokenize(grid)))
+	try:
+		tokens = tokenize(grid)
+		hand_sides = listsplit(tokens, eq)
+		return " = ".join(parse_hand_side(x) for x in hand_sides)
+	except:
+		return "error"
 
 import sys
 
-print(parse("""
-##..###....###
-.#..#.#....###
-....###....###
-"""))
+grids = open(sys.argv[1]).read().strip().split('\n')
+grids = listsplit(grids, '.' * len(grids[0]))
+grids = filter(len, grids)
+
+for grid in grids:
+	print(parse(grid))

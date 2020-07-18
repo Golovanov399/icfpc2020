13bb9e0c512456a496d174f4ee45c39b4314f5dd
Alexander Golovanov <Golovanov12345@gmail.com>
2020-07-18 22:47:47 +0300

    Add humanizer and modulator

---
+++ humanize.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+def convert_or_leave_it(a):
+	if a == "nil":
+		return []
+	elif isinstance(a, str):
+		return int(a)
+	elif isinstance(a, tuple):
+		assert len(a) == 2
+		l = convert_or_leave_it(a[0])
+		r = convert_or_leave_it(a[1])
+		if isinstance(r, list):
+			return [l] + r
+		else:
+			return (l, r)
+	else:
+		return a
+
+def modulate_int(n):
+	if n == 0:
+		return "010"
+	res = ""
+	if n >= 0:
+		res += "01"
+	else:
+		res += "10"
+		n = -n
+	sz = 0
+	while n + 1 > 16 ** sz:
+		sz += 1
+	res += '1' * sz + '0'
+	res += bin(n)[2:].rjust(4 * sz, '0')
+	return res
+
+def modulate(tokens):
+	res = ""
+	i = 0
+	while i < len(tokens):
+		if tokens[i:i+3] == ["ap", "ap", "cons"]:
+			res += "11"
+			i += 3
+		elif tokens[i] == "nil":
+			res += "00"
+			i += 1
+		else:
+			res += modulate_int(int(tokens[i]))
+			i += 1
+	return res
+
+def repr(tokens):
+	a = tokens[:]
+	for i in range(len(a) - 1, -1, -1):
+		while True:
+			if a[i] == "ap" and a[i + 1] == "ap" and a[i + 2] == "cons":
+				a = a[:i] + [(a[i + 3], a[i + 4])] + a[i + 5:]
+			else:
+				break
+	assert len(a) == 1
+	a = a[0]
+	return convert_or_leave_it(a)
+
+a = input().strip().split()
+print(*repr(a), sep='\n')
+print(modulate(a))

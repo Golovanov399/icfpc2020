b3226a1ce662e329d6004a2140548c6a3bd174cb
Nikita Mikhaylov <nikita.mikhaylov@aimtech.com>
2020-07-20 12:53:12 +0300

    add ship.py

---
+++ app/ship.py
@@ -0,0 +1,20 @@
+class Stats:
+
+    def __init__(self, stats):
+        self.energy = stats[0]
+        self.laser = stats[1]
+        self.power = stats[2]
+        self.max_clones = stats[3]
+
+
+class Ship:
+
+    def __init__(self, ship):
+        self.role = ship[0]
+        self.id = ship[1]
+        self.pos = ship[2]
+        self.vel = ship[3]
+        self.stats = Stats(ship[4])
+        self.temp = ship[5]
+        self.max_temp = ship[6]
+        self.cheat = ship[7]

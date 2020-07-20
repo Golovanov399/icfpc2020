ddbb6f6767f224b018d67d7cca489fa0f18eb406
unknown <endagorion@gmail.com>
2020-07-20 06:29:13 +0300

    I am a leaf on the wind, watch me soar

--- app/main.py
+++ app/main.py
@@ -82,7 +82,72 @@ def shoot(id, target, x):
 def clone(id, stats):
     return [3, id, list(stats)]
 
+goodStates = []
+
+def rotate(p):
+    x, y = p
+    return (y, -x)
+
+def sgn(x):
+    if x == 0:
+        return 0
+    if x > 0:
+        return 1
+    else:
+        return -1
+
+def gravity(p):
+    x, y = p
+    if abs(x) == abs(y):
+        return (-sgn(x), -sgn(y))
+    if abs(x) > abs(y):
+        return (-sgn(x), 0)
+    else:
+        return (0, -sgn(y))
+
+def vsum(p, q):
+    return (p[0] + q[0], p[1] + q[1])
+
+def genGoodStates(R):
+    global goodStates
+    p = (-R * R + R // 2, R * R + R // 2)
+    v = (R, R)
+    for i in range(2 * R):
+        for j in range(4):
+            goodStates += [(p, v)]
+            p = rotate(p)
+            v = rotate(v)
+        v = vsum(v, gravity(p))
+        p = vsum(p, v)
+
+def dist(a, b):
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+
+def dist2(a, b):
+    return dist(a[0], b[0]) + dist(a[1], b[1])
+
+def steer(p, v):
+    P = 10
+
+    bsc = 1000000
+    st = ()
+    for dx in range(-2, 3):
+        for dy in range(-2, 3):
+            sc = 1000000
+            nv = vsum(v, vsum(gravity(p), (dx, dy)))
+            np = vsum(p, nv)
+            for w in goodStates:
+                res = dist2((np, nv), w) + P * max(abs(dx), abs(dy))
+                sc = min(sc, res)
+            if sc < bsc:
+                bsc = sc
+                st = (-dx, -dy)
+    return st
+
 def main():
+    genGoodStates(8)
+    print(len(goodStates))
+
     server_url = sys.argv[1]
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
@@ -93,8 +158,7 @@ def main():
     print(resp)
     state = demodulate(resp.text)
     print(state)
-    params = [254, 0, 16, 1] if state[2][1] == 1 else [30, 96, 8, 1]
-    state = demodulate(requests.post(url, data=modulate([3, int(player_key), params]), params={"apiKey": "e8bdb469f76642ce9b510558e3d024d7"}).text)
+    state = demodulate(requests.post(url, data=modulate([3, int(player_key), [254,0,16,1]]), params={"apiKey": "e8bdb469f76642ce9b510558e3d024d7"}).text)
 
     while 1:
         print(state)
@@ -106,12 +170,19 @@ def main():
         our_role = staticInfo[1]
         our_ships = [x for x in gameState[2] if x[0][0] == our_role] if gameState else None
         cmds = []
+        '''
         for ship, _ in our_ships:
             x, y = ship[2]
             def sign(x):
                 return -1 if x < 0 else 1 if x > 0 else 0
             if abs(x) < 60 and abs(y) < 60:
-                cmds.append(accelerate(ship[1], (-sign(x) if abs(x) >= abs(y) else 0, -sign(y) if abs(y) > abs(x) else 0)))
+                cmds.append(accelerate(ship[1], (-sign(x) if abs(x) >= abs(y) else 0, -sign(y) if abs(y) > abs(x) else 0))
+        '''
+        for ship, _ in our_ships:
+            st = steer(ship[2], ship[3])
+            print(st)
+            if st[0] or st[1]:
+                cmds.append(accelerate(ship[1], st))
         state = demodulate(requests.post(url, data=modulate([4, int(player_key), cmds]), params={"apiKey": "e8bdb469f76642ce9b510558e3d024d7"}).text)
 
 if __name__ == '__main__':

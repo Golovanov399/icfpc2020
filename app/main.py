7b6dd73846a92981a76a63bf9a9b054fdd9345c0
unknown <endagorion@gmail.com>
2020-07-20 07:46:24 +0300

    More dodging

--- app/main.py
+++ app/main.py
@@ -186,7 +186,7 @@ def main():
             stats = ship[4]
             buf = ship[6] - ship[5]
             powah = stats[1]
-            dodge = our_role == 1 and 2 * buf < ship[6]
+            dodge = our_role == 1
             for eship, _ in their_ships:
                 t = vsum(vsum(eship[2], eship[3]), gravity(eship[2]))
                 D = dist(ship[2], t)

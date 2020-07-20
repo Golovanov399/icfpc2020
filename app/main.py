0235e3bb91b98c0bd51c0c568df2aefb83c3033a
unknown <endagorion@gmail.com>
2020-07-20 07:13:58 +0300

    IMMA FIRIN MAH LAZOR

--- app/main.py
+++ app/main.py
@@ -124,10 +124,10 @@ def dist(a, b):
     return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
 
 def dist2(a, b):
-    return dist(a[0], b[0]) + 10 * dist(a[1], b[1])
+    return dist(a[0], b[0]) + 2 * dist(a[1], b[1])
 
 def steer(p, v):
-    P = 100
+    P = 20
 
     bsc = 1000000
     st = ()
